1646876ff5ea0ed75aa119ce14e31a2293b9ccff
Ravikanth Marri <ravikanth.marri@gmail.com>
2023-12-20 17:12:10 +0000

    Initial Commit

---
+++ ProductAPISwiftUI/ProductAPISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductAPISwiftUIApp.swift
+//  ProductAPISwiftUI
+//
+//  Created by Ravikanth on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProductAPISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
